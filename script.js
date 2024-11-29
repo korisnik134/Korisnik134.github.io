4b83d7235076e05b266d38b41aeddf20f6baef2f
korisnik134 <eleskovic96@gmail.com>
2024-11-30 00:05:49 +0100

    Update script.js
    Version 3.0

--- script.js
+++ script.js
@@ -39,8 +39,8 @@ buttons.forEach(button => {
         try {
           console.log(`Pre obračuna: ${expression}`);
 
-          // Obrada procenata - ovde pretvaramo procenat u odnos sa prethodnim brojem
-          let evalExpression = expression.replace(/(\d+)%/g, " * ($1 / 100)");
+          // Obrada procenata - kada je pritisnut procenat, tretiramo ga kao odnos sa prethodnim brojem
+          let evalExpression = expression.replace(/(\d+)%/g, " * ($1 / 100)"); // Korekcija: PROCENAT ZAMENJUJEMO ISPRAVNO
 
           // Dodavanje podrške za početne operatore
           if (/^[+\-*/]/.test(evalExpression)) {
